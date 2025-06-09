099c2e9e04645b3e55ff61d3e2387e2415e22dc6
Acha <achacbmb3@gmail.com>
2025-06-09 11:16:04 +0200

    [Test] ErrorBoundary

---
+++ frontend/src/components/common/ErrorBoundary.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import ErrorBoundary from "./ErrorBoundary";
+
+const BadChartChild = () => {
+  throw new Error("I am not drawing!");
+};
+
+describe("ErrorBoundary", () => {
+  test("renders children without error", () => {
+    render(
+      <ErrorBoundary>
+        <div data-testid="child">Hello world</div>
+      </ErrorBoundary>
+    );
+    expect(screen.getByTestId("child")).toBeInTheDocument();
+  });
+
+  test("renders message instead of child when the later throws error", () => {
+    // https://krasimirtsonev.com/blog/article/jest-mock-console-methods + ChatGPT
+    const consoleErrorSpy = jest
+      .spyOn(console, "error")
+      .mockImplementation(() => {});
+
+    render(
+      <ErrorBoundary>
+        <BadChartChild />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Check parameters/i)).toBeInTheDocument();
+  });
+});
